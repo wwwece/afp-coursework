24560b9bae2e307da6d24e67bacb4b2087152c02
WeceW <toni.weckroth@gmail.com>
2019-02-27 20:30:32 +0200

    Tasks 1, 2 & 4 ("is", "not is", "where is") seems to work

--- WeckrothToni_coursework.hs
+++ WeckrothToni_coursework.hs
@@ -23,40 +23,63 @@ askQuestions story = do
 
 -- TODO: TYPE SIGNATURE
 -- answer :: Foldable t => [[Char]] -> t String -> IO ()
-answer question@(w1:w2:words) story
-  | w1' == "is"                     = putStrLn $ case (answerIs question story) of 
-                                                      Just True  -> "yes"
-                                                      Just False -> "no"
-                                                      Nothing    -> "maybe"
-  | w1' == "where" && w2' == "is"   = putStrLn $ w1 ++ w2
-  | w1' == "where" && w2' == "was"  = putStrLn $ w1 ++ w2
-  | w1' == "how"   && w2' == "many" = putStrLn $ w1 ++ w2
-  | w1' == "how"   && w2' == "do"   = putStrLn $ w1 ++ w2
+answer question@(word1:word2:_) story
+  | w1 == "is"                     = putStrLn $ answerIs question story
+  | w1 == "where" && w2 == "is"   = putStrLn $ answerWhereIs question story
+  | w1 == "where" && w2 == "was"  = putStrLn $ word1 ++ word2
+  | w1 == "how"   && w2 == "many" = putStrLn $ word1 ++ word2
+  | w1 == "how"   && w2 == "do"   = putStrLn $ word1 ++ word2
   | otherwise                     = putStrLn "I don't understand!"
-  where w1' = map toLower w1
-        w2' = map toLower w2
+  where w1 = map toLower word1
+        w2 = map toLower word2
 
 
--- Pattern in IS-questions: (Is:Name:In:The:Location:?)
-answerIs :: Foldable t => [String] -> t String -> Maybe Bool
+-- Pattern in IS-questions: (is:name:in:the:location:?)
+-- answerIs :: Foldable t => [String] -> t String -> Maybe Bool
 answerIs (_:name:_:_:location:_) story = 
-  foldl (\acc statement -> 
+  foldl (\answer statement -> 
         let s     = words $ map toLower statement
             name' = map toLower name 
             loc   = map toLower location
-        in if name' `elem` s
-           then if loc `elem` s && not ("no" `elem` s) && not ("either" `elem` s)
-                then Just True -- location found without negation or either statement
-                else if ("no" `elem` s && loc `elem` s) || (not ("no" `elem` s) && not ("either" `elem` s))
-                     then Just False -- not in this particular location OR in some other location
-                     else if "no" `elem` s && not (loc `elem` s)
-                          then Nothing -- No longer in some other location, so can possibly be in this location.
-                          else if "either" `elem` s && loc `elem` s
-                               then Nothing -- Is either in this location, or some other location
-                               else acc
-           else acc) Nothing story
-answerIs _ _ = Nothing
+        in  if name' `elem` s
+            then if loc `elem` s && not ("no" `elem` s) && not ("either" `elem` s)
+                 then "yes" -- location found without negation or either statement
+                 else if ("no" `elem` s && loc `elem` s) || (not ("no" `elem` s) && not ("either" `elem` s))
+                      then "no" -- not in this particular location OR in some other location
+                      else if "no" `elem` s && not (loc `elem` s)
+                           then "maybe" -- No longer in some other location, so can possibly be in this location.
+                           else if "either" `elem` s && loc `elem` s
+                                then "maybe" -- Is either in this location, or some other location
+                                else answer
+            else answer
+  ) "maybe" story
+answerIs _ _ = "maybe"
 
+
+-- TODO: TYPE SIGNATURE
+-- Pattern in WHERE-questions (where:is:the:item:?)
+answerWhereIs (_:_:_:item:_) story = 
+  whereIsPerson (foldl (\person statement -> 
+                       let s = words $ map toLower statement
+                           item' = map toLower item
+                       in  if item' `elem` s
+                           then Just (head $ words statement) -- Name of the person posessing the item
+                           else person
+                ) Nothing story) story
+answerWhere _ _ = "don't know"
+
+
+-- Checks where is a person with a given name
+-- TODO: TYPE SIGNATURE
+whereIsPerson Nothing _ = "don't know"
+whereIsPerson (Just name) story = 
+  foldl (\loc statement -> 
+        let (word1:word2:_) = words $ map toLower statement 
+            name' = map toLower name
+        in  if name' == word1 && word2 `elem` ["moved","went","journeyed"]
+            then last $ words statement
+            else loc
+  ) "don't know" story
 
 
 -- ----------------------- --
