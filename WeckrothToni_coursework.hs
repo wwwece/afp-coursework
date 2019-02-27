04c8db65747cb4c745a834bb5df7a136efb475e1
WeceW <toni.weckroth@gmail.com>
2019-02-27 19:10:09 +0200

    Some refactoring

--- WeckrothToni_coursework.hs
+++ WeckrothToni_coursework.hs
@@ -1,6 +1,7 @@
 
 import System.Environment
 import System.IO
+import Data.Char (toLower)
 
 main :: IO ()
 main = do 
@@ -22,33 +23,38 @@ askQuestions story = do
 
 -- TODO: TYPE SIGNATURE
 -- answer :: Foldable t => [[Char]] -> t String -> IO ()
-answer question@(q1:q2:qs) story
-  | q1 == "Is"                    = putStrLn $ case (answerIs question story) of 
-                                                    Just True  -> "yes"
-                                                    Just False -> "no"
-                                                    Nothing    -> "maybe"
-  | q1 == "Where" && q2 == "is"   = putStrLn $ q1 ++ q2
-  | q1 == "Where" && q2 == "was"  = putStrLn $ q1 ++ q2
-  | q1 == "How"   && q2 == "many" = putStrLn $ q1 ++ q2
-  | q1 == "How"   && q2 == "do"   = putStrLn $ q1 ++ q2
-  | otherwise                     = putStrLn "maybe"
+answer question@(w1:w2:words) story
+  | w1' == "is"                     = putStrLn $ case (answerIs question story) of 
+                                                      Just True  -> "yes"
+                                                      Just False -> "no"
+                                                      Nothing    -> "maybe"
+  | w1' == "where" && w2' == "is"   = putStrLn $ w1 ++ w2
+  | w1' == "where" && w2' == "was"  = putStrLn $ w1 ++ w2
+  | w1' == "how"   && w2' == "many" = putStrLn $ w1 ++ w2
+  | w1' == "how"   && w2' == "do"   = putStrLn $ w1 ++ w2
+  | otherwise                     = putStrLn "I don't understand!"
+  where w1' = map toLower w1
+        w2' = map toLower w2
 
 
 -- Pattern in IS-questions: (Is:Name:In:The:Location:?)
 answerIs :: Foldable t => [String] -> t String -> Maybe Bool
 answerIs (_:name:_:_:location:_) story = 
-  foldl (\acc statement -> let s = (words statement) in
-                           if name `elem` s
-                           then if location `elem` s && not ("no" `elem` s) && not ("either" `elem` s)
-                                then Just True -- location found without negation or either statement
-                                else if ("no" `elem` s && location `elem` s) || (not ("no" `elem` s) && not ("either" `elem` s))
-                                     then Just False -- not in this particular location OR in some other location
-                                     else if "no" `elem` s && not (location `elem` s)
-                                          then Nothing -- No longer in some other location, so can possibly be in this location.
-                                          else if "either" `elem` s && location `elem` s
-                                               then Nothing -- Is either in this location, or some other location
-                                               else acc
-                           else acc) Nothing story
+  foldl (\acc statement -> 
+        let s     = words $ map toLower statement
+            name' = map toLower name 
+            loc   = map toLower location
+        in if name' `elem` s
+           then if loc `elem` s && not ("no" `elem` s) && not ("either" `elem` s)
+                then Just True -- location found without negation or either statement
+                else if ("no" `elem` s && loc `elem` s) || (not ("no" `elem` s) && not ("either" `elem` s))
+                     then Just False -- not in this particular location OR in some other location
+                     else if "no" `elem` s && not (loc `elem` s)
+                          then Nothing -- No longer in some other location, so can possibly be in this location.
+                          else if "either" `elem` s && loc `elem` s
+                               then Nothing -- Is either in this location, or some other location
+                               else acc
+           else acc) Nothing story
 answerIs _ _ = Nothing
 
 
@@ -64,6 +70,6 @@ getStory = do
   return $ lines file
 
 prettyPrintList :: [String] -> IO ()
-prettyPrintList []     = do putStrLn "> End of story.\n"
-prettyPrintList (x:xs) = do putStrLn x
+prettyPrintList []     = do putStrLn "> END OF STORY.\n"
+prettyPrintList (x:xs) = do putStrLn ("  " ++ x)
                             prettyPrintList xs
